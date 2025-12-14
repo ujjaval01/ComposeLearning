03d91aa257cd2526b5bd26dfd9015aa2ef936ec6
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-15 03:54:25 +0530

    inital commit

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/JetpackCompose.kt
@@ -0,0 +1,9 @@
+package com.uv.jetpackcomposelearning.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Greet(){
+    Text("Hello Android")
+}
