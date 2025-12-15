625f6fc07bed43901d657a7a078a755b75e3b151
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-16 02:42:08 +0530

    practice of data types

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/functions/dataTypeExample.kt
@@ -0,0 +1,11 @@
+package com.uv.jetpackcomposelearning.ui.kotlinLearning.functions
+
+fun main() {
+    val age = 22              // Int
+    val price = 99.99         // Double
+    val isActive = true       // Boolean
+    val grade = 'A'           // Char
+    val name = "Ujvl"         // String
+
+    println("$age $price $isActive $grade, $name")
+}
