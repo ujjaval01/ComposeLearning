165d545cfa544b0de29a9f2da8ab8b105a14aa80
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-16 03:24:42 +0530

    performing when statement

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/functions/whenPractice.kt
@@ -0,0 +1,18 @@
+package com.uv.jetpackcomposelearning.ui.kotlinLearning.functions
+
+fun main(){
+    print("Enter the day num: ")
+    val day = readln().toInt()
+
+    when(day){
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Invalid day")
+
+    }
+}
