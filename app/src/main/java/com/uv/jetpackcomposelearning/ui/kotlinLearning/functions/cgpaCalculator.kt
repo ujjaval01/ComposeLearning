271d44c5299d051f52425b34482fc804e7601aa3
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-16 17:52:13 +0530

    Playing with functions

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/functions/cgpaCalculator.kt
@@ -0,0 +1,41 @@
+package com.uv.jetpackcomposelearning.ui.kotlinLearning.functions
+
+fun main() {
+    print("Enter Hindi: ")
+    val hindi = readln().toInt()
+
+    print("Enter English: ")
+    val english = readln().toInt()
+
+    print("Enter Math: ")
+    val math = readln().toInt()
+
+    print("Enter Science: ")
+    val science = readln().toInt()
+
+    print("Enter Social: ")
+    val social = readln().toInt()
+
+    print("Enter Art: ")
+    val art = readln().toInt()
+
+    calCgpa(hindi, english, math, science, social, art)
+}
+
+fun calCgpa(
+    hindi: Int,
+    english: Int,
+    math: Int,
+    science: Int,
+    social: Int,
+    art: Int
+) {
+    val totalMarks = hindi + english + math + science + social + art
+    val cgpa = (totalMarks.toDouble() / 600) * 10
+    println("Your cgpa is %.2f".format(cgpa))
+    /*%d → Int
+    %s → String
+    %f → Float/Double
+    %.2f → decimal control
+    .format() → clean & professional output*/
+}
