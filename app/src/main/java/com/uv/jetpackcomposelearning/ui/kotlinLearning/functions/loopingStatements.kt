1e137f311aa4e7fb742ec4e6a54cda5a1ba17da5
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-16 09:59:42 +0530

    practice of looping statements

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/functions/loopingStatements.kt
@@ -0,0 +1,19 @@
+package com.uv.jetpackcomposelearning.ui.kotlinLearning.functions
+
+fun main(){
+//    for (i in 1..10){
+//        println("2 * $i = ${i*2}")
+//    }
+
+//    var i = 1;
+//    while(i<=10){
+//        println("2 * $i = ${i*2}")
+//        i++
+//    }
+
+    var i = 1
+    do{
+        println("2 * $i = ${i*2}")
+        i++
+    }while(i<=10)
+}
