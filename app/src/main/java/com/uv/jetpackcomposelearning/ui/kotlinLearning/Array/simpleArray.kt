11ff25de5edbab301aa82e355dcb1a7ca48692d4
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-17 16:57:14 +0530

    practice of more array inbuilt methods

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/Array/simpleArray.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/Array/simpleArray.kt
@@ -2,18 +2,35 @@ package com.uv.jetpackcomposelearning.ui.kotlinLearning.Array
 
 fun main(){
     val arr = arrayOf(1,2,3,4,5)
+
+//    arr.sortDescending()
+
+
+
     // first method
+    print("Using java foreach loop: ")
     for(ele in arr){
         print("$ele ")
     }
     println()
     // second method
+    print("Using simple loop: ")
     for(i in 0..arr.size-1){
         print("${arr[i]} ")
+
     }
+    println()
     // third method
+    print("Using indices: ")
     for(e in arr.indices){
-        println(e)
+        print("$e ")
     }
+    println()
+    // fourth method
+    print("Using foreach: ")
+    arr.forEach {
+        print("$it ")
+    }
+
 
 }
