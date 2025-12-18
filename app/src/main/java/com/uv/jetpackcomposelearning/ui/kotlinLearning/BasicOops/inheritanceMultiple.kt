2dc311ddbea3b852cba227991a97d060db6973af
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-18 18:37:09 +0530

    solving diamond problem using explicitly

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/BasicOops/inheritanceMultiple.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/BasicOops/inheritanceMultiple.kt
@@ -1,31 +1,50 @@
 package com.uv.jetpackcomposelearning.ui.kotlinLearning.BasicOops
 
-interface OpenCamera{
-    fun clickPic()
-}
-interface OpenFlash{
-    fun flashOn()
-}
-
-class Camera : OpenCamera, OpenFlash{
-    override fun clickPic() {
-        println("Picture clicked...")
-    }
-
-    override fun flashOn() {
-        println("Flash turned on...")
-    }
-    fun captureVideo(){
-        println("Video captured...")
-    }
-    fun all(){
-        clickPic()
-        flashOn()
-        captureVideo()
+// correct way of multiple Inheritance...
+interface A {
+    fun show() {
+        println("A show")
     }
 }
 
-fun main(){
-    val c = Camera()
-    c.all()
+interface B {
+    fun show() {
+        println("B show")
+    }
 }
+
+class C : A, B {
+    override fun show() {
+        super<A>.show()
+        super<B>.show()
+    }
+}
+
+fun main() {
+    val obj = C()
+    obj.show()
+}
+
+
+//  ambiguity occurs in this code...
+//interface WithFlash{
+//    fun clickPic(){
+//        println("Picture clicked...")
+//    }
+//}
+//interface WithoutFlash{
+//    fun clickPic(){
+//        println("Flash turned on...")
+//    }
+//}
+//class Camera : WithFlash, WithoutFlash{
+//    fun clickPicture(){
+//        super.clickPic()  // ambiguity occurs..
+//        super.clickPic()
+//    }
+//}
+//
+//fun main(){
+//    val c = Camera()
+//    c.clickPicture()
+//}
