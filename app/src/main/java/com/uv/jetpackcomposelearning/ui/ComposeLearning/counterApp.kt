3174ba557fafd3cd2281eb9272ba417a66f8c25d
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-21 11:22:42 +0530

    Learning work of spacer

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/counterApp.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/counterApp.kt
@@ -1,6 +1,10 @@
 package com.uv.jetpackcomposelearning.ui.ComposeLearning
 
 import android.widget.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,20 +12,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.uv.jetpackcomposelearning.R
 
 @Composable
 fun CounterScreen() {
     var count by remember { mutableIntStateOf(0) }
     var reset = 0
-    Text("$count")
+    Column {
+        Text("$count", modifier = Modifier.padding(20.dp), color = colorResource(R.color.teal_200))
 
-    Button(onClick =  {
-        count++
-    }){
-        Text("Increment")
+        // space between two components
+        Spacer(modifier = Modifier.height(10.dp))
+        // Button for increment
+        Button(onClick =  {
+            count++
+        }){
+            Text("Increment", modifier = Modifier.padding(10.dp), color = colorResource(R.color.purple_200))
+        }
+
+        // space between two components
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            count = reset
+        }) { Text("Reset") }
     }
-    Button(onClick = {
-        count = reset
-    }) { Text("Reset") }
+
 
 }
