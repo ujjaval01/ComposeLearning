22f371f6e803807003d06d65745681b7fecbbd6e
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-23 20:37:30 +0530

    practice of row and cols

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/RowAndCol.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/RowAndCol.kt
@@ -3,18 +3,38 @@ package com.uv.jetpackcomposelearning.ui.ComposeLearning
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RowAndCol(){
-    Row {
-        Text("Ujjaval")
-        Text("Vanshika")
-
+//    Row (
+//        modifier = Modifier.fillMaxSize(),
+//        horizontalArrangement = Arrangement.Center,
+//        verticalAlignment = Alignment.CenterVertically
+//    ){
+//        Text("Ujjaval")
+//        Text("Vanshika")
+//    }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Ujjaval",
+            modifier = Modifier.padding(16.dp)
+            )
+        Text(
+            text = "Vanshika",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 
 
