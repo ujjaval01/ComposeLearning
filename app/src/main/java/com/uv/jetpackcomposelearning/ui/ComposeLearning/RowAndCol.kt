6dff44ec6f8d5e49a9080f7652dd7a76c0a852b3
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-23 21:10:35 +0530

    practice of row and cols

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/RowAndCol.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/ComposeLearning/RowAndCol.kt
@@ -1,16 +1,32 @@
 package com.uv.jetpackcomposelearning.ui.ComposeLearning
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
 
 @Composable
 fun RowAndCol(){
@@ -28,13 +44,53 @@ fun RowAndCol(){
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = "Ujjaval",
-            modifier = Modifier.padding(16.dp)
-            )
+            text = "Rounded",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            color = Color.Black,
+            letterSpacing = 2.sp,   // space between letters..
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.Yellow)
+                .padding(8.dp)
+        )
+        Text(
+            text = "Very very very very very very very very very very very very very very very very very long text...",
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
         Text(
             text = "Vanshika",
+            style = androidx.compose.ui.text.TextStyle(
+                shadow = Shadow(
+                    color = Color.Gray,
+                    offset = Offset(2f, 2f),
+                    blurRadius = 4f
+                )
+            ),
+            fontSize = 20.sp,
             modifier = Modifier.padding(16.dp)
         )
+
+        Text(
+            text = "Ujjaval Saini",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.Black)
+                .clickable { },
+            color = Color.White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+
     }
 
 
