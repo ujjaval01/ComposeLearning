f9ce6e0b01fe6770e191a0c31d2e96b287409fff
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-29 22:02:15 +0530

    ;earning about lazyCAolums

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/rowsAndCols/ListExample.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/rowsAndCols/ListExample.kt
@@ -1,12 +1,23 @@
 package com.uv.jetpackcomposelearning.ui.kotlinLearning.rowsAndCols
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListExample(modifier: Modifier = Modifier) {
+    val names = listOf("sagar, suraj, ujvl, anuj," +
+            " amit, aashish, aman, raja, aadi, ujjaval," +
+            " aditya, sorav , heer, ranjha, gorav, annu, asmhit"
+    )
 
+    LazyColumn {
+        items(names.size) {
+            Text(text = names[it])
+        }
+    }
 }
 
 @Composable
