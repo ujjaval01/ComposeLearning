3b1f4281aa5bcccf68c9392bdeb7efb940f871b4
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-29 22:24:38 +0530

    ;earning about lazyCAolums

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/rowsAndCols/ListExample.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/kotlinLearning/rowsAndCols/ListExample.kt
@@ -1,21 +1,33 @@
 package com.uv.jetpackcomposelearning.ui.kotlinLearning.rowsAndCols
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ListExample(modifier: Modifier = Modifier) {
-    val names = listOf("sagar, suraj, ujvl, anuj," +
-            " amit, aashish, aman, raja, aadi, ujjaval," +
-            " aditya, sorav , heer, ranjha, gorav, annu, asmhit"
+    val names = listOf(
+        "Sagar", "Suraj", "Ujvl", "Anuj",
+        "Amit", "Aashish", "Aman", "Raja",
+        "Aadi", "Ujjaval", "Aditya", "Sorav",
+        "Heer", "Ranjha", "Gorav", "Annu", "Ashmit", "Sagar", "Suraj", "Ujvl", "Anuj",
+        "Amit", "Aashish", "Aman", "Raja",
+        "Aadi", "Ujjaval", "Aditya", "Sorav",
+        "Heer", "Ranjha", "Gorav", "Annu", "Ashmit"
     )
+    LazyColumn(
 
-    LazyColumn {
-        items(names.size) {
-            Text(text = names[it])
+    ) {
+        items(names.size){ value->  // index-> explicit, it-> implicit
+            Text(names[value], modifier = Modifier.padding(18.dp))
         }
     }
 }
