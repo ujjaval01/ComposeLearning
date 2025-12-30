481f06878901aa4ca8902e3b019a3dbd2a414168
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-30 12:05:17 +0530

    learning about navigation with navigationLibrary3

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/navigationLibrary/ScreenA.kt
@@ -0,0 +1,16 @@
+package com.uv.jetpackcomposelearning.ui.navigationLibrary
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ScreenA(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ScreeAPrev() {
+    ScreenA()
+}
