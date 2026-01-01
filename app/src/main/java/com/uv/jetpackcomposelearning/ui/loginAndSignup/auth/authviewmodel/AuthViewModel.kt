451350ec7c5f3ecf5a5d616617140edac1a96000
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-01 12:40:38 +0530

    Refactor login and signup methods to provide error messages on failure.

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/loginAndSignup/auth/authviewmodel/AuthViewModel.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/loginAndSignup/auth/authviewmodel/AuthViewModel.kt
@@ -4,19 +4,36 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel : ViewModel() {
+
     private val auth = FirebaseAuth.getInstance()
 
-    fun login(email: String, password: String, onResult:(Boolean) -> Unit){
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            onResult(task.isSuccessful)
-        }
+    fun login(
+        email: String,
+        password: String,
+        result: (Boolean, String) -> Unit
+    ) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    result(true, "")
+                } else {
+                    result(false, task.exception?.message ?: "Login failed")
+                }
+            }
     }
 
-    fun signup(email: String, password: String, onResult: (Boolean) -> Unit){
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            onResult(task.isSuccessful)
-        }
+    fun signup(
+        email: String,
+        password: String,
+        result: (Boolean, String) -> Unit
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    result(true, "")
+                } else {
+                    result(false, task.exception?.message ?: "Signup failed")
+                }
+            }
     }
-
-
 }
