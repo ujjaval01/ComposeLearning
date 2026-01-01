d1436b6e7435a2c2a7464ad903e6ab3b16a0b023
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-01 12:48:38 +0530

    Add animated text to the splash screen for enhanced user experience.

--- app/src/main/java/com/uv/jetpackcomposelearning/ui/foodOrderingApp/SplashScreen.kt
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/foodOrderingApp/SplashScreen.kt
@@ -1,17 +1,23 @@
 package com.uv.jetpackcomposelearning.ui.foodOrderingApp
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
@@ -51,8 +57,23 @@ fun FoodSplashScreen(
         LottieAnimation(
             composition = composition,
             progress = { progress },
-            modifier = Modifier.size(220.dp)
+            modifier = Modifier
+                .size(220.dp)
+                .align(Alignment.Center)
         )
+
+        val alpha by animateFloatAsState(
+            targetValue = 1f,
+            animationSpec = tween(1200)
+        )
+
+        Text("Food App with Compose",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 90.dp)
+            )
     }
 }
 
