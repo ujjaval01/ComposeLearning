9a152b33e9a8f84d8980e95e314ca5bafcce291b
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-01 12:47:09 +0530

    Add ScreenRoutes object to centralize navigation constants.

---
+++ app/src/main/java/com/uv/jetpackcomposelearning/ui/loginAndSignup/screens/ScreenRoutes.kt
@@ -0,0 +1,8 @@
+package com.uv.jetpackcomposelearning.ui.loginAndSignup.screens
+
+object ScreenRoutes {
+//    const val SPLASH = "food_splash"
+    const val LOGIN = "screen_login"
+    const val SIGNUP = "screen_signup"
+    const val HOME = "food_home"
+}
